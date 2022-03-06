9520ed72958a9d29cc7e0dfc17f43c07707421f5
Prateek Kumar Singh <20cs01003@iitbbs.ac.in>
2022-03-06 21:48:24 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const express = require("express");
 dotenv.config({path: "./.env"});
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
